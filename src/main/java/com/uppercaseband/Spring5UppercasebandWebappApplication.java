7bb83bb2be2d5455cdaa5e4607f4884e3f1ba735
markyMARK <markdeleon@Mark-Rhoss-Borruel-De-Leons-MacBook-Pro.local>
2018-11-21 16:05:49 -0500

    Initial commit - Spring5 webapp for UPPERCASE band website

---
+++ src/main/java/com/uppercaseband/Spring5UppercasebandWebappApplication.java
@@ -0,0 +1,16 @@
+package com.uppercaseband;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author markdeleon
+ *
+ */
+@SpringBootApplication
+public class Spring5UppercasebandWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5UppercasebandWebappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/uppercaseband/Spring5UppercasebandWebappApplicationTests.java
@@ -0,0 +1,21 @@
+package com.uppercaseband;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * @author markdeleon
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5UppercasebandWebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
